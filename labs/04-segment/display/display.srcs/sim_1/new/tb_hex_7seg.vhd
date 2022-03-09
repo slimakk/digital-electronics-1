ec9200011ce68667a46eb3fdc2526202883d51ad
student <student@PC-636.urel.feec.vutbr.cz>
2022-03-09 14:53:28 +0100

    LAB4 update

--- labs/04-segment/display/display.srcs/sim_1/new/tb_hex_7seg.vhd
+++ labs/04-segment/display/display.srcs/sim_1/new/tb_hex_7seg.vhd
@@ -1,10 +1,34 @@
-# Lab 4: Miroslav Babeľa
+library ieee;
+use ieee.std_logic_1164.all;
 
-### Seven-segment display decoder
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_hex_7seg is
+    -- Entity of testbench is always empty
+end entity tb_hex_7seg;
 
-1. Listing of VHDL stimulus process from testbench file (`tb_hex_7seg.vhd`) with asserts. Verify all input combinations. Always use syntax highlighting, meaningful comments, and follow VHDL guidelines:
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_hex_7seg is
 
-```vhdl
+    -- Local signals
+    signal s_hex  : std_logic_vector(4 - 1 downto 0);
+    signal s_seg  : std_logic_vector(7 - 1 downto 0);
+
+begin
+    -- Connecting testbench signals with decoder entity
+    -- (Unit Under Test)
+    uut_hex_7seg : entity work.hex_7seg
+        port map(
+            hex_i => s_hex,
+            seg_o => s_seg
+        );
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
     p_stimulus : process
     begin
         report "Stimulus process started" severity note;
@@ -77,48 +101,5 @@
         report "Stimulus process finished" severity note;
         wait;
     end process p_stimulus;
-```
 
-2. Screenshot with simulated time waveforms. Always display all inputs and outputs (display the inputs at the top of the image, the outputs below them) at the appropriate time scale!
-
-   ![your figure](wave.png)
-
-### LED(7:4) indicators
-
-1. Listing of LEDs(7:4) part of VHDL architecture from source file `top.vhd`. Try to write logic functions as simple as possible. Always use syntax highlighting, meaningful comments, and follow VHDL guidelines:
-
-   ```vhdl
-   --------------------------------------------------------------------
-   -- Experiments on your own: LED(7:4) indicators
-
-   -- Turn LED(4) on if input value is equal to 0, ie "0000"
-   -- LED(4) <= `0` when WRITE YOUR CODE HERE
-
-   -- Turn LED(5) on if input value is greater than "1001", ie 10, 11, 12, ...
-   -- LED(5) <= WRITE YOUR CODE HERE
-
-   -- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
-   -- LED(6) <= WRITE YOUR CODE HERE
-
-   -- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
-   -- LED(7) <= WRITE YOUR CODE HERE
-   ```
-### Truth table
-| **Hex** | **Inputs** | **LED4** | **LED5** | **LED6** | **LED7** |
-   | :-: | :-: | :-: | :-: | :-: | :-: |
-   | 0 | 0000 |  |  |  |  |
-   | 1 | 0001 |  |  |  |  |
-   | 2 | 0010 |  |  |  |  |
-   | 3 | 0011 |  |  |  |  |
-   | 4 | 0100 |  |  |  |  |
-   | 5 | 0101 |  |  |  |  |
-   | 6 | 0110 |  |  |  |  |
-   | 7 | 0111 |  |  |  |  |
-   | 8 | 1000 |  |  |  |  |
-   | 9 | 1001 |  |  |  |  |
-   | A | 1010 |  |  |  |  |
-   | b | 1011 |  |  |  |  |
-   | C | 1100 |  |  |  |  |
-   | d | 1101 |  |  |  |  |
-   | E | 1110 |  |  |  |  |
-   | F | 1111 |  |  |  |  |
+end architecture testbench;
